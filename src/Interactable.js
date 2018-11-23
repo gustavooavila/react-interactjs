21226fffec368d8f5602849cc9d4f11d5ccd4f14
Shadownrun <gustavooavila@gmail.com>
2018-11-23 16:56:33 -0200

    refactor, add resizeMove event

--- src/Interactable.js
+++ src/Interactable.js
@@ -17,8 +17,8 @@ export default class Interactable extends Component {
 	}
 
 	render() {
-		return cloneElement(this.props.children, { 
-			ref: node => this.node = node, 
+		return cloneElement(this.props.children, {
+			ref: node => this.node = node,
 			draggable: false
 		})
 	}
@@ -38,23 +38,29 @@ export default class Interactable extends Component {
 		if (this.props.resizable) this.interact.resizable(this.props.resizableOptions)
 		if (this.props.dropzone) this.interact.dropzone(this.props.dropzoneOptions)
 		if (this.props.gesturable) this.interact.gesturable(this.props.gesturableOptions)
-		if (this.props.tap) this.interact.on('tap',this.props.tap)
-		if (this.props.doubletap) this.interact.on('tap',this.props.doubletap)
-		if (this.props.hold) this.interact.on('tap',this.props.hold)
+
+		if (this.props.tap) this.interact.on('tap', this.props.tap)
+		if (this.props.hold) this.interact.on('hold', this.props.hold)
+		if (this.props.doubleTap) this.interact.on('doubletap', this.props.doubleTap)
+		if (this.props.resizemove) this.interact.on('resizemove', this.props.resizeMove)
 	}
 }
 
 Interactable.propTypes = {
 	children: React.PropTypes.node.isRequired,
+
 	draggable: React.PropTypes.bool,
-	draggableOptions: React.PropTypes.object,
 	resizable: React.PropTypes.bool,
-	resizableOptions: React.PropTypes.object,
 	dropzone: React.PropTypes.bool,
-	dropzoneOptions: React.PropTypes.object,
 	gesturable: React.PropTypes.bool,
+
+	draggableOptions: React.PropTypes.object,
+	resizableOptions: React.PropTypes.object,
+	dropzoneOptions: React.PropTypes.object,
 	gesturableOptions: React.PropTypes.object,
+
 	tap: React.PropTypes.func,
-	doubletap: React.PropTypes.func,
-	hold: React.PropTypes.func
+	hold: React.PropTypes.func,
+	doubleTap: React.PropTypes.func,
+	resizeMove: React.PropTypes.func
 }
